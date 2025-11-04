00c31f5779e280dcb4f13e9df9839b0782d4ab0f
sonelu <alex.sonea@gmail.com>
2025-11-04 15:34:18 +0000

    gym env updates

--- envs/mh5robotenv.py
+++ envs/mh5robotenv.py
@@ -1,16 +1,21 @@
+import logging
+from typing import Callable
 import numpy as np
+from numpy.typing import NDArray
+
+import mujoco
 from gymnasium.envs.mujoco import MujocoEnv
 from gymnasium.spaces import Box
 import pathlib
 
-DEFAULT_CAMERA_CONFIG = {
-    "trackbodyid": 1,
-    "distance": 1.0,
-    # "lookat": np.array((0.0, 0.0, 0.25)),
-    "elevation": -20.0,
-    "azimuth": 150,
+# DEFAULT_CAMERA_CONFIG = {
+#     "trackbodyid": 1,
+#     "distance": 2.0,
+#     # "lookat": np.array((0.0, 0.0, 0.25)),
+#     "elevation": -20.0,
+#     "azimuth": 150,
 
-}
+# }
 
 
 class MH5RobotEnv(MujocoEnv):
@@ -28,21 +33,26 @@ class MH5RobotEnv(MujocoEnv):
     def __init__(
             self,
             model_path: str = "assets/scene.xml",
+            # this will allow more stability in calculations, but will still
+            # be in line with the actual robot 200Hz control cycle
+            # dt = timestep * frame_skip = 0.001 * 5 = 0.005 => 200Hz
+            timestep: float = 0.001,
             frame_skip: int = 5,
             forward_reward_weight: float = 1.25,
             ctrl_cost_weight: float = 0.1,
             contact_cost_weight: float = 5e-7,
             contact_cost_range: tuple[float, float] = (-np.inf, 10.0),
-            healthy_reward: float = 5.0,
+            healthy_reward: float = 10.0,
             terminate_when_unhealthy: bool = True,
-            healthy_z_range: tuple[float, float] = (0.12, 0.5),
+            healthy_z_range: tuple[float, float] = (0.16, 0.25),
             reset_noise_scale: float = 3e-3,
             exclude_current_positions_from_observation: bool = True,
-            include_cinert_in_observation: bool = True,
-            include_cvel_in_observation: bool = True,
-            include_qfrc_actuator_in_observation: bool = True,
-            include_cfrc_ext_in_observation: bool = True,
-            default_camera_config = DEFAULT_CAMERA_CONFIG,
+            include_cinert_in_observation: bool = False,
+            include_cvel_in_observation: bool = False,
+            include_qfrc_actuator_in_observation: bool = False,
+            include_cfrc_ext_in_observation: bool = False,
+            orientation: Callable[[], NDArray] | None = None,
+            # default_camera_config = DEFAULT_CAMERA_CONFIG,
             **kwargs
         ):
 
@@ -62,12 +72,14 @@ class MH5RobotEnv(MujocoEnv):
         self._include_qfrc_actuator_in_observation = include_qfrc_actuator_in_observation
         self._include_cfrc_ext_in_observation = include_cfrc_ext_in_observation
 
+        self._orientation = orientation
+
         MujocoEnv.__init__(
             self,
             model_path=f"{pathlib.Path(__file__).parent.resolve()}/{model_path}",
             frame_skip=frame_skip,
             observation_space=None,
-            default_camera_config = default_camera_config,
+            # default_camera_config = default_camera_config,
             **kwargs
         )
 
@@ -81,6 +93,8 @@ class MH5RobotEnv(MujocoEnv):
         self.observation_space = Box(
             low=-np.inf, high=np.inf, shape=(obs_size,), dtype=np.float32
         )
+
+        self.model.opt.timestep = timestep
 
     @property
     def is_healthy(self):
@@ -124,10 +138,15 @@ class MH5RobotEnv(MujocoEnv):
             )
         )
 
-    def _get_rew(self, x_velocity: float, action):
-        forward_reward = self._forward_reward_weight * x_velocity
-        healthy_reward = self._healthy_reward
-        rewards = forward_reward + healthy_reward
+    def _get_rew(self, x_velocity: float, z_position: float, action):
+        # forward reward only if upright
+        if z_position >= self._healthy_z_range[0]:
+            forward_reward = self._forward_reward_weight * x_velocity
+        else:
+            forward_reward = 0.0
+
+        position_reward = self._healthy_reward * z_position
+        rewards = forward_reward + position_reward
 
         ctrl_cost = self.control_cost(action)
         contact_cost = self.contact_cost
@@ -136,7 +155,7 @@ class MH5RobotEnv(MujocoEnv):
         reward = rewards - costs
 
         reward_info = {
-            "reward_survive": healthy_reward,
+            "reward_position": position_reward,
             "reward_forward": forward_reward,
             "reward_ctrl": -ctrl_cost,
             "reward_contact": -contact_cost,
@@ -163,6 +182,9 @@ class MH5RobotEnv(MujocoEnv):
         return contact_cost
 
     def step(self, action):
+        if np.isnan(np.sum(action)):
+            logging.getLogger("mh5_env").error(f"action contains nan: {action}")
+            raise ValueError(f"action contains nan: {action}")
         xy_position_before = self.mass_center()
         self.do_simulation(action, self.frame_skip)
         xy_position_after = self.mass_center()
@@ -171,8 +193,16 @@ class MH5RobotEnv(MujocoEnv):
         x_velocity, y_velocity = xy_velocity
 
         observation = self._get_obs()
-        reward, reward_info = self._get_rew(x_velocity, action)
-        terminated = (not self.is_healthy) and self._terminate_when_unhealthy
+        if np.isnan(np.sum(observation)):
+            logging.getLogger("mh5_env").error(f"observation contains nan: {observation}")
+            raise ValueError(f"observation contains nan: {observation}")
+        reward, reward_info = self._get_rew(x_velocity, observation[2], action)
+        if np.isnan(reward):
+            logging.getLogger("mh5_env").error(f"reward is nan: {reward}")
+            raise ValueError(f"reward is nan: {reward}")
+        # terminated = (not self.is_healthy) and self._terminate_when_unhealthy
+        # we do not stop the rollout when the robot is down; we want it to learn to stand up
+        terminated = False
         info = {
             "x_position": self.data.qpos[0],
             "y_position": self.data.qpos[1],
@@ -193,7 +223,11 @@ class MH5RobotEnv(MujocoEnv):
         noise_low = -self._reset_noise_scale
         noise_high = self._reset_noise_scale
 
-        qpos = self.init_qpos + self.np_random.uniform(
+        qpos = self.init_qpos
+        if self._orientation is not None:
+            orient = self._orientation()
+            qpos[3:7] = orient
+        qpos = qpos + self.np_random.uniform(
             low=noise_low, high=noise_high, size=self.model.nq
         )
         qvel = self.init_qvel + self.np_random.uniform(
